d8fd9d9a889b5cdfaee17129efedcde60f80613e
jenn <um.jennifer@gmail.com>
2019-04-17 21:38:18 -0700

    fix suggestions made on april15, 2019

--- fun.py
+++ fun.py
@@ -195,10 +195,10 @@ class Fun:
             yield (pixels, 0.1, False)
 
     def fade(self):
-        whiile True:
+        while True:
             bright = 255
             pixels = [(bright,bright,bright)] * numLEDs
             while bright > 0:
                 bright -= 1
-            pixels = [(bright,bright,bright)] * numLEDs
-            yield (pixels, 3, False)
+                pixels = [(bright,bright,bright)] * numLEDs
+                yield (pixels, .027, False)
